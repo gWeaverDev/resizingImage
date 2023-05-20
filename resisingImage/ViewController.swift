cff22b3052b832086c28dd084de490f50161c65a
George Weaver <george.weaver.dev@icloud.com>
2023-05-20 13:16:05 +0300

    changed on frames and set viewDidLayoutSubviews

--- resisingImage/ViewController.swift
+++ resisingImage/ViewController.swift
@@ -9,76 +9,79 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private var heightBannerConstraint: NSLayoutConstraint?
+    enum Constants {
+        static var imageViewHeight: CGFloat = 270
+    }
     
-    private let bannerImage: UIImageView = {
-        let imageView = UIImageView(image: UIImage(named: "blackLord"))
+    private var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "poster1")
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
-        imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
     
-    private let scrollView: UIScrollView = {
-        let scroll = UIScrollView()
-        scroll.backgroundColor = .white
-        scroll.showsVerticalScrollIndicator = true
-        scroll.indicatorStyle = .black
-        scroll.translatesAutoresizingMaskIntoConstraints = false
-        scroll.contentSize = CGSize(width: scroll.contentSize.width, height: 1000)
-        scroll.automaticallyAdjustsScrollIndicatorInsets = true
-        return scroll
+    private var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        scrollView.contentInsetAdjustmentBehavior = .never
+        return scrollView
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAppearance()
-        setupLayout()
-        navigationController?.setNavigationBarHidden(true, animated: false)
-        scrollView.delegate = self
+        setupViews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.contentInset = UIEdgeInsets(
+            top: Constants.imageViewHeight,
+            left: 0,
+            bottom: scrollView.safeAreaInsets.bottom,
+            right: 0
+        )
+        scrollView.contentOffset = CGPoint(x: 0, y: -scrollView.contentInset.top)
+        scrollView.contentSize = CGSize(width: view.frame.width, height: 1000)
+        
+        imageView.frame = CGRect(
+            x: 0,
+            y: -Constants.imageViewHeight,
+            width: scrollView.bounds.width,
+            height: Constants.imageViewHeight
+        )
+            
     }
     
     private func setupAppearance() {
         view.backgroundColor = .white
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
     
-    private func setupLayout() {
-        view.addSubview(scrollView)
-        view.addSubview(bannerImage)
-        
-        heightBannerConstraint = bannerImage.heightAnchor.constraint(equalToConstant: 400)
-        
-        NSLayoutConstraint.activate([
-            
-            bannerImage.topAnchor.constraint(equalTo: view.topAnchor),
-            bannerImage.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            bannerImage.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            heightBannerConstraint!,
-            
-            scrollView.topAnchor.constraint(equalTo: bannerImage.topAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-        ])
-    }
+    private func setupViews() {
+           
+       scrollView.frame = view.bounds
+       scrollView.delegate = self
+       
+       scrollView.addSubview(imageView)
+       view.addSubview(scrollView)
+   }
 
 }
 
 extension ViewController: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let y = 400 + (scrollView.contentOffset.y - 400)
-        heightBannerConstraint?.constant = 400 - y
-        scrollView.verticalScrollIndicatorInsets.top = 400 - y
-        scrollView.verticalScrollIndicatorInsets.bottom = y / 2
+        let offsetY = scrollView.contentOffset.y
+        
+        if offsetY < -Constants.imageViewHeight {
+            var frame = imageView.frame
+            frame.origin.y = offsetY
+            frame.size.height = -offsetY
+            imageView.frame = frame
+            scrollView.verticalScrollIndicatorInsets.top = -offsetY
+        }
     }
-    
-//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-//        if heightBannerConstraint!.constant < 65 {
-//            UIView.animate(withDuration: 0.8, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5) {
-//                self.heightBannerConstraint?.constant = 300
-//            }
-//        }
-//    }
 }
-
